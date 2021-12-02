a814fae93c529ab013986da273b3e5660e09739c
Daniel Petranek <d@petranek.net>
2021-12-02 12:29:11 -0600

    Day 2

--- src/aoc/aoc_2021.clj
+++ src/aoc/aoc_2021.clj
@@ -77,3 +77,69 @@
 
 
   ,,,)
+
+
+(defn load-input
+  [filename]
+  (->> (slurp (io/resource filename))
+       (str/split-lines)))
+
+(comment
+  (def in2 (load-input "2_1.txt"))
+
+  (def data2 (->> in2
+                  (map (fn [s] (str/split s #" ")))
+                  (map (fn [[cmd magnitude]] [cmd (Integer/parseInt magnitude)]))))
+
+
+  (->> data2
+       (reduce (fn [[progress depth] [cmd dist]]
+                 (case cmd
+                   "forward" [(+ progress dist) depth]
+                   "backward" [(- progress dist) depth]
+                   "up" [progress (- depth dist)]
+                   "down" [progress (+ depth dist)])
+                 )
+               [0 0])
+       (apply *))
+  2120749
+  [2053 1033]
+
+  (def ex2 [["forward" 5]
+            ["down" 5]
+            ["forward" 8]
+            ["up" 3]
+            ["down" 8]
+            ["forward" 2]])
+
+  (defn navigate [{:keys [progress depth aim] :as pos} [cmd dist]]
+    (println (pr-str pos) (pr-str [cmd dist]))
+    (case cmd
+      "forward" (-> pos
+                    (assoc :progress (+ progress dist))
+                    (assoc :depth (+ depth (* aim dist))))
+      "up" (-> pos
+               #_(assoc :depth (- depth dist))
+               (assoc :aim (- aim dist)))
+      "down" (-> pos
+                 #_(assoc :depth (+ depth dist))
+                 (assoc :aim (+ aim dist)))))
+
+  (->> ex2
+       (reduce navigate {:depth 0 :progress 0 :aim 0}))
+  {:depth 60, :progress 15, :aim 10}
+  {:depth 60, :progress 15}
+
+  (let [{:keys [depth progress]} (reduce navigate {:depth 0 :progress 0 :aim 0} data2)]
+    (* depth progress))
+  2138382217
+  {:depth 1041589, :progress 2053, :aim 1033}
+  {:depth 3099, :progress 2053, :aim 1033}
+  (* 3099 2053)
+  6362247
+
+
+
+
+
+  ,,,)
