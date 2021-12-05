926eeda3262cf4056bca920a5a6645cb957f9ac6
Daniel Petranek <d@petranek.net>
2021-12-05 00:05:49 -0600

    Day 4 - Cleanup and tests

--- src/aoc/y2021/day4.clj
+++ src/aoc/y2021/day4.clj
@@ -3,27 +3,6 @@
             [clojure.string :as str]
             [clojure.set :as set]))
 
-(def ex-bingo
-  "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
-
-22 13 17 11  0
- 8  2 23  4 24
-21  9 14 16  7
- 6 10  3 18  5
- 1 12 20 15 19
-
- 3 15  0  2 22
- 9 18 13 17  5
-19  8  7 25 23
-20 11 10 24  4
-14 21 16 12  6
-
-14 21 17 24  4
-10 16 15  9 19
-18  8 23 26 20
-22 11 13  6  5
- 2  0 12  3  7")
-
 (defn parse-boards
   [raw-board-strings]
   (loop [[this & remaining] raw-board-strings
@@ -49,6 +28,12 @@
       (str/split #",")
       (->> (map #(Integer/parseInt %)))))
 
+(defn raw->edn
+  [strings]
+  (let [[raw-nums _ & raw-boards] strings]
+    {:numbers (parse-nums raw-nums)
+     :boards (parse-boards raw-boards)}))
+
 (defn transpose
   [matrix]
   (apply map vector matrix ))
@@ -57,11 +42,12 @@
   "Takes a board and a set of called nums, returns sum of unmarked-numbers if a winner,
   nil if loser."
   [nums board]
+  ;; build a sequence of row sets and col sets
   (let [winners (map set (concat board (transpose board)))]
+    ;; if the nums are a superset of any of them, that's bingo!
     (when (some (partial set/superset? nums) winners)
       ;; sum up the uncalled numbers
-      (apply + (set/difference (set (mapcat identity board))
-                               nums)))))
+      (reduce + (set/difference (set (flatten board)) nums)))))
 
 (defn play-bingo
   [nums boards]
@@ -76,28 +62,10 @@
           (recur remaining (conj called callout)))))))
 
 (comment
-  ;; winning: any of 5 cols, any of 5 rows
-  ;; set of rows and cols
-
-  (let [[raw-nums _ & raw-boards] (str/split-lines ex-bingo)]
-    (def ex-nums (parse-nums raw-nums))
-    (def ex-boards (parse-boards raw-boards)))
-
-  (play-bingo ex-nums ex-boards)
-  4512
-
-
-  ,)
-
-
-(comment
-  (let [[raw-nums _ & raw-boards] (ut/load-input "4_1.txt")]
-    (def bingo-nums (parse-nums raw-nums))
-    (def boards (parse-boards raw-boards)))
-
+  (def input (raw->edn (ut/load-input "4_1.txt")))
   ;; part1
 
-  (play-bingo bingo-nums boards)
+  (play-bingo (:numbers input) (:boards input))
   67716
 
 
@@ -110,7 +78,6 @@
          prev-called #{}
          boards (set boards)]
     (let [called (conj prev-called callout)]
-      (println callout (count called) (count boards))
       (cond (nil? callout)
             :game-over
 
@@ -120,6 +87,7 @@
               (* callout bingo!)
               ;; last board hasn't won yet...
               (recur remaining called boards))
+
             :else
             (if-let [bingos (filter (partial winner called) boards)]
               ;; get rid of all the winners
@@ -128,14 +96,9 @@
               (recur remaining called boards))))))
 
 (comment
-  (lose-bingo ex-nums ex-boards)
-  1924
+  ;; part 2
 
-  (lose-bingo bingo-nums boards)
+  (lose-bingo (:numbers input) (:boards input))
   1830
-  :game-over
 
-
-
-
-  )
+  ,)
